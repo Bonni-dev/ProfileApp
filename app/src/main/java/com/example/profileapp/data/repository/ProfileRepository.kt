28e21e44bdd00417109592c1d680d5a29c4a4761
Bononi <53324114+Bonnivaldo@users.noreply.github.com>
2024-08-17 18:08:50 -0300

    create data source

---
+++ app/src/main/java/com/example/profileapp/data/repository/ProfileRepository.kt
@@ -0,0 +1,21 @@
+package com.example.profileapp.data.repository
+
+import com.example.profileapp.data.database.entities.ProfileEntity
+import com.example.profileapp.data.model.Profile
+import com.example.profileapp.ui.registration.RegistrationViewParams
+
+interface ProfileRepository {
+
+    suspend fun createProfile(registrationViewParams: RegistrationViewParams)
+
+    suspend fun getProfile(id: Int): Profile
+
+    suspend fun updateProfile(profile: Profile)
+
+    suspend fun getAllProfiles(): List<Profile>
+
+    suspend fun getActiveProfile(): List<ProfileEntity>
+
+    suspend fun getInactiveProfiles(): List<ProfileEntity>
+
+}
---
+++ app/src/main/java/com/example/profileapp/data/repository/ProfileRepositoryImpl.kt
@@ -0,0 +1,44 @@
+package com.example.profileapp.data.repository
+
+import com.example.profileapp.data.database.dao.ProfileDao
+import com.example.profileapp.data.database.entities.ProfileEntity
+import com.example.profileapp.data.database.entities.toProfile
+import com.example.profileapp.data.database.entities.toProfileEntity
+import com.example.profileapp.data.model.Profile
+import com.example.profileapp.ui.registration.RegistrationViewParams
+
+class ProfileRepositoryImpl(
+    private val profileDao: ProfileDao
+): ProfileRepository {
+
+    override suspend fun createProfile(registrationViewParams: RegistrationViewParams) {
+        val profileEntity = registrationViewParams.toProfileEntity()
+        profileDao.insertProfile(profileEntity)
+    }
+
+    override suspend fun getProfile(id: Int): Profile {
+        return profileDao.getProfileById(id).toProfile()
+    }
+
+    override suspend fun updateProfile(profile: Profile) {
+        val profileEntity = profile.toProfileEntity()
+        profileDao.updateProfile(profileEntity)
+    }
+
+    override suspend fun getAllProfiles(): List<Profile> {
+        val profileList = profileDao.getAllProfiles()
+        return profileList.map { it.toProfile() }
+    }
+
+    override suspend fun getActiveProfile(): List<ProfileEntity> {
+        val activeProfile = profileDao.getAllProfiles()
+        activeProfile.map { it.toProfile() }
+        return activeProfile.filter { it.activeUser }
+    }
+
+    override suspend fun getInactiveProfiles(): List<ProfileEntity> {
+        val activeProfile = profileDao.getAllProfiles()
+        activeProfile.map { it.toProfile() }
+        return activeProfile.filter { it.activeUser == false }
+    }
+}
